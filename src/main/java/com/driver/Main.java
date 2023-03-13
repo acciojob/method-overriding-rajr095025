fbb9afd66db2123fcf18c968849350d7abc0f737
jaya vignesh <rajr095025@gmail.com>
2023-03-13 23:58:48 +0530

    changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,7 +7,17 @@ public class Main {
     }
 
 }
+class A{
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
 
+class B extends A {
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+    }
+}
 /**
 
  Method Overriding
